a98e73e17f18c04b7c6d2bc24a3577701c967f7f
GabrielIcban <icbangabriel99@gmail.com>
2024-08-20 01:12:00 +0800

    aboutme-fixed script

--- Gabriel_Icban_Portfolio/script.js
+++ Gabriel_Icban_Portfolio/script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+
+
+// script.js
+let currentIndex = 0;
+const images = document.querySelectorAll('.about-pic');
+
+function showNextImage() {
+    // Hide the current image
+    images[currentIndex].classList.remove('visible');
+    
+    // Update the index to the next image
+    currentIndex = (currentIndex + 1) % images.length;
+    
+    // Show the next image
+    images[currentIndex].classList.add('visible');
+}
+
+// Initial display
+images[currentIndex].classList.add('visible');
+
+// Change image every 2 seconds
+setInterval(showNextImage, 2000);
